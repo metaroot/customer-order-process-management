8d391cfbe001429b66a166e9d7e5a8a7117e72cb
metaroot <faisal.ahmed.cse.sust@gmail.com>
2021-05-30 16:46:18 +0600

    Customer UI and admin UI

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4af24a57cc3399f3ce4c40577bd8d298'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e91b6922-e4b7-4f14-a016-58a832be8abc'
+};
